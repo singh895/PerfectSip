9469e3f84b5753aa36016c9ca3b3190ce7a53af0
singh895 <singh895@purdue.edu>
2024-02-26 08:39:31 -0500

    Initial Commit

---
+++ PerfectSip/NavigationBar.swift
@@ -0,0 +1,42 @@
+//
+//  NavigationBar.swift
+//  hm19
+//
+//  Created by Saanvi Singh on 12/02/24.
+//
+
+import Foundation
+
+import CoreBluetooth
+
+import SwiftUI
+
+extension View {
+    func navigationBarColor(backgroundColor: Color, titleColor: Color) -> some View {
+        modifier(NavigationBarModifier(backgroundColor: backgroundColor, titleColor: titleColor))
+    }
+}
+
+struct NavigationBarModifier: ViewModifier {
+    var backgroundColor: Color
+    var titleColor: Color
+
+    init(backgroundColor: Color, titleColor: Color) {
+        self.backgroundColor = backgroundColor
+        self.titleColor = titleColor
+
+        let coloredAppearance = UINavigationBarAppearance()
+        coloredAppearance.configureWithOpaqueBackground()
+        coloredAppearance.backgroundColor = UIColor(backgroundColor)
+        coloredAppearance.titleTextAttributes = [.foregroundColor: UIColor(titleColor)]
+        coloredAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor(titleColor)]
+
+        UINavigationBar.appearance().standardAppearance = coloredAppearance
+        UINavigationBar.appearance().scrollEdgeAppearance = coloredAppearance
+    }
+
+    func body(content: Content) -> some View {
+        content
+    }
+}
+
---
+++ PerfectSip/PerfectSipApp.swift
@@ -0,0 +1,17 @@
+//
+//  PerfectSipApp.swift
+//  PerfectSip
+//
+//  Created by Saanvi Singh on 16/02/24.
+//
+
+import SwiftUI
+
+@main
+struct PerfectSipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
